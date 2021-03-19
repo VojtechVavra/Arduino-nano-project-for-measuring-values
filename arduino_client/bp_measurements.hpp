4e94701c6d4e64478c004117c7c1aa96f04d1437
Lord Vooja, the Guardian <60519144+VojtechVavra@users.noreply.github.com>
2021-03-19 22:16:57 +0100

    Update bp_measurements.hpp

--- arduino_client/bp_measurements.hpp
+++ arduino_client/bp_measurements.hpp
@@ -1,14 +1,13 @@
 #ifndef BP_MEASUREMENTS_HPP
 #define BP_MEASUREMENTS_HPP
 
-struct Measurements { // celkem | 40 + 1 (null terminator)
+struct Measurements { // celkem | 35 + 1 (null terminator)
     float weight;       // Kg   | 5
     float temperature;  // °C   | 5
     float pressure;     // hPa  | 5
     float humidity;     // %    | 5
     float altitude;     // m    | 5
-    unsigned long int time;  // | 10
-    //time_t time;
+    unsigned long int time;  // | 10 // time_t
 };
 
 #endif    // !BP_MEASUREMENTS_HPP
